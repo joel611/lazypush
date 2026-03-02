6cff4cd61a6ee63f46dba4491cbd6998d5d3f899
Joel Chan <chanhokyim@gmail.com>
2026-03-02 18:44:06 +0800

    feat: bootstrap bun + @opentui/solid entry point

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import { render } from "@opentui/solid"
+
+const App = () => (
+  <box style={{ flexDirection: "column", width: "100%", height: "100%" }}>
+    <text>lazypush starting...</text>
+  </box>
+)
+
+render(() => <App />)
