13666cb1df72dc6f5bc88157d3544ed9f5ab0821
Joel Chan <chanhokyim@gmail.com>
2026-03-02 18:45:19 +0800

    feat: update StatusBar hints for 5-panel layout

--- src/components/StatusBar.tsx
+++ src/components/StatusBar.tsx
@@ -3,25 +3,17 @@ import { Show } from "solid-js"
 
 export const StatusBar = () => {
   const isModal = () => modal().type !== "none"
-  const isProjects = () => focused() === "projects"
 
   return (
-    <box
-      style={{
-        width: "100%",
-        flexDirection: "column",
-        padding: 1,
-        backgroundColor: "#222222",
-      }}
-    >
+    <box style={{ width: "100%", padding: 1, backgroundColor: "#222222" }}>
       <Show when={!isModal()}>
         <text style={{ color: "#888888" }}>
-          <span style={{ color: "#00FFFF" }}>tab</span>:switch
-          <span style={{ color: "#00FFFF" }}> n</span>:new-project
-          <span style={{ color: "#00FFFF" }}> a</span>:add-device
+          <span style={{ color: "#00FFFF" }}>tab</span>:focus
+          <span style={{ color: "#00FFFF" }}> n</span>:new
           <span style={{ color: "#00FFFF" }}> e</span>:edit
           <span style={{ color: "#FF4444" }}> D</span>:delete
-          <span style={{ color: "#00FFFF" }}> m</span>:message
+          <span style={{ color: "#00FFFF" }}> m</span>:compose
+          <span style={{ color: "#FFAA00" }}> t</span>:template
           <span style={{ color: "#00FF00" }}> s</span>:send
           <span style={{ color: "#FF4444" }}> q</span>:quit
         </text>
